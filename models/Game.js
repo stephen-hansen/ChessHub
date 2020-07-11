d5a3e519a87794352864abc055fe3d45f7a0f5b6
stevinh <sph77@drexel.edu>
2020-07-11 16:09:42 -0400

    Fix eslint errors, begin adding dependencies

--- models/Game.js
+++ models/Game.js
@@ -2,31 +2,33 @@ import Chess from 'chess.js';
 import React from 'react';
 
 export default class Game extends React.Component {
-   constructor() {
-      this.board = new Chess();
-      this.players = [];
-      this.turn = 'w';
-   }
+  constructor() {
+    super();
+    this.board = new Chess();
+    this.players = [];
+    this.turn = 'w';
+  }
 
-   addPlayer(p) {
-      this.players.append(p);
-   }
+  addPlayer(p) {
+    this.players.append(p);
+  }
 
-   move(m) {
-      let success = this.board.move(m);
-      if (success) {
-         if (this.turn == 'w') {
-            this.turn == 'b';
-         } else {
-            this.turn == 'w';
-         }
-         return true;
+  move(m) {
+    const success = this.board.move(m);
+    if (success) {
+      if (this.turn === 'w') {
+        this.turn = 'b';
       } else {
-         return false;
+        this.turn = 'w';
       }
-   }
+      return true;
+    }
+    return false;
+  }
 
-   render() {
-      //TODO
-   }
+  /*
+  render() {
+    // TODO
+  }
+  */
 }
--- models/Player.js
+++ models/Player.js
@@ -1,13 +1,15 @@
-import Chess from 'chess.js';
 import React from 'react';
 
 export default class Player extends React.Component {
-   constructor(c, id) {
-      this.color = c;
-      this.socketId = id;
-   }
+  constructor(c, id) {
+    super();
+    this.color = c;
+    this.socketId = id;
+  }
 
-   render() {
-      //TODO
-   }
+  /*
+  render() {
+    // TODO
+  }
+  */
 }
